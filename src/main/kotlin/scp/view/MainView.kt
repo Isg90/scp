4507026fd3e7f694ad5410f6e19de11854d50b4d
isg90 <isg90@mail.ru>
2020-03-09 18:06:04 +0300

    add buttons images

--- src/main/kotlin/scp/view/MainView.kt
+++ src/main/kotlin/scp/view/MainView.kt
@@ -1,16 +1,22 @@
 package scp.view
 
-import javafx.scene.control.Button
+import javafx.geometry.Orientation
 import javafx.scene.layout.VBox
-import javafx.scene.text.Font
+import javafx.scene.text.FontWeight
 import scp.io.FileSystemService
 import scp.model.item.BlockItem
 import scp.model.item.ButtonItem
 import scp.model.item.MainItem
 import tornadofx.*
 
-class MainView : View("SaveCopyPast v1.0") {
-    val controller: MyController by inject()
+val defaultDimensionUnit = Dimension.LinearUnits.px
+val defaultHgrap = Dimension(10.0, defaultDimensionUnit)
+val defaultVgrap = Dimension(10.0, defaultDimensionUnit)
+val labelFontSize = Dimension(20.0, defaultDimensionUnit)
+const val applicationName = "SaveCopyPast v1.0"
+
+class MainView : View(applicationName) {
+    private val controller: MyController by inject()
     override val root = vbox {}
 
     init {
@@ -21,34 +27,69 @@ class MainView : View("SaveCopyPast v1.0") {
 
     private fun buildMainView(mainItem: MainItem) {
         root.apply {
+            style {
+                vgap = defaultVgrap
+            }
             mainItem.blockItems.forEach {
-                buildBlock(it)
+                add(buildBlock(it))
             }
         }
     }
 
     private fun buildBlock(block: BlockItem): VBox {
         return vbox {
-            label {
-                text = block.name
-                font = Font.font(20.0)
+            borderpane {
+                left {
+                    label(block.name) {
+                        style {
+                            fontWeight = FontWeight.BOLD
+                            fontSize = labelFontSize
+                        }
+                    }
+                }
+                right {
+                    add(buildImageButton("button/002-pencil.png", {
+
+                    }))
+                }
             }
+
             flowpane {
+                orientation = Orientation.HORIZONTAL
+                style {
+                    hgap = defaultHgrap
+                    vgap = defaultVgrap
+                }
                 block.buttonItems.forEach {
-                    buildButton(it)
+                    add(buildButton(it))
                 }
             }
         }
     }
 
-    private fun buildButton(buttonItem: ButtonItem) : Button =
-        button {
-            text = buttonItem.name
+    private fun buildButton(buttonItem: ButtonItem) =
+        button(buttonItem.name) {
             action {
                 onLeftClick {
                     clipboard.putString(buttonItem.value)
                 }
             }
+        }
+
+    private fun buildImageButton(imagePath: String, actionBlock: () -> Unit) =
+        button {
+            imageview(imagePath) {
+                fitHeight= 20.0
+                fitWidth= 20.0
+            }
+            action {
+                actionBlock()
+            }
+//            imageview(Image(FileInputStream(resources[imagePath]))) {
+//                style {
+//                    size = labelFontSize
+//                }
+//            }
         }
 }
 
@@ -57,7 +98,7 @@ class MyFragment : Fragment() {
 }
 
 class MyController : Controller() {
-    val fsService = FileSystemService.instance
+    private val fsService = FileSystemService.instance
 
     fun getMainItem() =
         MainItem().apply {
